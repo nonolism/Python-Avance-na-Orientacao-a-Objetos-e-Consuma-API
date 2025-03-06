0afe6538d36261a12425e680513d103b806087d1
Nonolism <ruanvitormoreno@outlook.com>
2025-03-06 16:25:09 -0300

    Aula 3 - Acessando os itens do cardápio

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
-restaurante_praca.receber_avaliacao('Gui', 10)
-restaurante_praca.receber_avaliacao('Lais', 8)
-restaurante_praca.receber_avaliacao('Emy', 2)
+bebida_suco = Bebida('Suco de Melancia', 5.0, 'Grande')
+prato_paozinho = Prato('Paozinho', 2.0, 'O melhor pão da cidade')
 
 def main():
-    Restaurante.listar_restaurantes()
+    pass
 
 if __name__ == '__main__':
     main()
