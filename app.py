856cdd5d385a094d3327756f5b90c7342928a9ef
Nonolism <ruanvitormoreno@outlook.com>
2025-03-09 20:46:15 -0300

    Aula 3 - Métodos para adicionar itens

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ restaurante_praca = Restaurante('praça', 'Gourmet')
 bebida_suco = Bebida('Suco de Melancia', 5.00, 'Grande')
 prato_paozinho = Prato('Pãozinho', 3.00, 'O melhor pão da cidade')
 
+restaurante_praca.adicionar_bebida_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_prato_no_cardapio(prato_paozinho)
+
 
 def main():
     pass
