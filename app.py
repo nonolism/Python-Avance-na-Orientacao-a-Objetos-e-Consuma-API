91ee4fa75b8deef472745199833e0f2a5f7f9f1a
Nonolism <ruanvitormoreno@outlook.com>
2025-03-09 23:54:09 -0300

    Aula 4 - Refatoração

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ restaurante_praca = Restaurante('praça', 'Gourmet')
 bebida_suco = Bebida('Suco de Melancia', 5.00, 'Grande')
 prato_paozinho = Prato('Pãozinho', 3.00, 'O melhor pão da cidade')
 
-restaurante_praca.adicionar_bebida_no_cardapio(bebida_suco)
-restaurante_praca.adicionar_prato_no_cardapio(prato_paozinho)
+restaurante_praca.adicionar_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_no_cardapio(prato_paozinho)
 
 
 def main():
