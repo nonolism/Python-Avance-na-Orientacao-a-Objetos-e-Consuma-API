1f870f31e3abf64302f9ff4949766695e519f3f1
Nonolism <ruanvitormoreno@outlook.com>
2025-06-12 18:36:33 -0300

    metodo abstrato e polimorfismo

--- modelos/cardapio/item_cardapio.py
+++ modelos/cardapio/item_cardapio.py
@@ -1,4 +1,11 @@
-class ItemCardapio:
+from abc import ABC, abstractmethod
+
+class ItemCardapio(ABC):
     def __init__(self, nome: str, preco: float):
         self._nome = nome
         self._preco = preco
+        
+    @abstractmethod
+    def aplicar_desconto(self):
+        """Método abstrato para aplicar desconto no item do cardápio."""
+        pass
